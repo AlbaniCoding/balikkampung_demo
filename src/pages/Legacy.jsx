6a30d76caf4ff679f7b9bbba570c0cf442585cea
AlbaniCoding <albani.kalamhaq2016@gmail.com>
2026-06-07 19:18:15 +0700

    Update Legacy.jsx

--- src/pages/Legacy.jsx
+++ src/pages/Legacy.jsx
@@ -1,12 +1,6 @@
-import { useEffect } from "react";
-
 export default function Legacy() {
   useEffect(() => {
-    fetch("/home.html")
-      .then(res => res.text())
-      .then(html => {
-        document.body.innerHTML = html;
-      });
+    window.location.href = "/home.html";
   }, []);
 
   return null;
